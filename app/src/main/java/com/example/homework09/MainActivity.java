e3c000621a5adb8290e37778f8a0bc85281f19f6
av4195 <av4195@bs.amalnet.k12.il>
2025-04-05 19:45:03 +0300

    עיצוב, אישור לאינטנט וקישורים

--- app/src/main/java/com/example/homework09/MainActivity.java
+++ app/src/main/java/com/example/homework09/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.homework09;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnGo;
+    WebView wV;
+    EditText eT;
+    String stringUrl;
+    String Adress;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btnGo = findViewById(R.id.btnGo);
+        eT = findViewById(R.id.eT);
+
+
+
+    }
+    public void Go(View view) {
+
     }
 }
